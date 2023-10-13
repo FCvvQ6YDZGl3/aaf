4643610f162b365792aed349d538d3eb632b736f
Попов Петр <popov.petr.viktorovich@gmail.com>
2023-10-13 12:29:18 +0700

    Разобрал приемы на C#, подавляющее большинство ясны.

---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+        public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
+        public decimal? Price { get; set; }
+        public Product Related { get; set; }
+        public bool InStock { get; }
+        public bool NameBeginWithS => Name?[0] == 'S';
+        public static Product[] GetProducts()
+        {
+            Product[] products = new Product[]
+            {
+                new Product
+                {
+                    Name = "Kayak",
+                    Category = "Water Craft",
+                    Price = 275M
+                },
+                new Product(false)
+                {
+                    Name = "Lifejacket",
+                    Price = 48.95M
+                }
+            };
+            return products;
+        }
+    }
+}
