7a637983c990865613a016a92e00586aab4de035
Попов Петр <popov.petr.viktorovich@gmail.com>
2023-10-18 16:02:04 +0700

    Вопроизвел примеры из седьмой главы. Нашел ошибку (опечатку) при создании тестовых данных для теста класса HomeControllerTests.cs

---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio.Tests/ProductTest.cs
@@ -0,0 +1,24 @@
+﻿using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class ProductTest
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            var p = new Product { Name = "Test", Price = 100M };
+            p.Name = "New Name";
+            Assert.Equal("New Name", p.Name);
+        }
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            const decimal price = 200M;
+            var p = new Product { Name = "Test", Price = 100M };
+            p.Price = price;
+            Assert.Equal(price, p.Price);
+        }
+    }
+}
